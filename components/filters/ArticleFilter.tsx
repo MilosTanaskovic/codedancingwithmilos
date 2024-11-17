c6d1c136df149b166a8f5bc20394994f30982e26
MilosTanaskovic <milostanaskovic.dev.data@gmail.com>
2024-11-17 13:53:20 +0100

    -d

--- components/filters/ArticleFilter.tsx
+++ components/filters/ArticleFilter.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { FilterItem } from "@/types/filter";
 import { filterData } from "@/data/filter";
+import Filter from "./Filter";
 
 interface ArticleFilterProps {
   selectedTopic: string;
